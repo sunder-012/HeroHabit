b5d1a98f081f1ccdd6b44b5d6254db5b0e51596c
sunder-012 <jagoba1207@gmail.com>
2023-11-14 14:17:57 +0100

    UPDATE: WIP
    Added ProgressBar functionality, DB in progress

--- app/src/main/java/com/example/herohabit/TaskListActivity.java
+++ app/src/main/java/com/example/herohabit/TaskListActivity.java
@@ -2,7 +2,9 @@ package com.example.herohabit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -15,7 +17,7 @@ import android.widget.Toast;
 import static com.example.herohabit.NewTaskActivity.NEW_TASK;
 
 public class TaskListActivity extends AppCompatActivity {
-
+    SQLiteDatabase db;
     private static final int MAX_RADIOBUTTONS = 7;
     public static final int TASK_LIST = 1;
     private RadioGroup radioGroup = null;
@@ -23,7 +25,9 @@ public class TaskListActivity extends AppCompatActivity {
     private RadioButton radioButton = null;
     private int progress = 0;
     private int level = 1;
-    private TextView lvl = null;
+    private int next = 30;
+    private TextView lvlView = null;
+    private TextView nextLvlView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,14 @@ public class TaskListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tasklist);
 
         radioGroup = findViewById(R.id.radioGroup);
+
         progressBar = findViewById(R.id.progressBar);
+        //SELECT XP IN PROGRESS progressBar.setProgress()
+        // progress =
+        progressBar.setMax(30);
+
+        //SELECT LVL level =
+        //SELECT nextLVL next =
 
         findViewById(R.id.addTask).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,9 +70,13 @@ public class TaskListActivity extends AppCompatActivity {
 
     private void increaseLevel(){
         Toast.makeText(getBaseContext(), "You reached a new level!", Toast.LENGTH_SHORT).show();
-        lvl = findViewById(R.id.lvl);
+        lvlView = findViewById(R.id.lvl);
         level++;
-        lvl.setText(level);
+        lvlView.setText(level);
+        // UPDATE level
+        nextLvlView = findViewById(R.id.nextLvl);
+        next = next - level;
+        nextLvlView.setText(next);
     }
 
     private void newTaskActivity(View view) {
@@ -73,15 +88,16 @@ public class TaskListActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (/*requestCode == CODE && */resultCode == RESULT_OK) {
-            // Get the data returned from NewTaskActivity
-            String param1 = data.getStringExtra("param1");
+        if (resultCode == RESULT_OK) {
+
+            db=openOrCreateDatabase("TaskDatabase.db", Context.MODE_PRIVATE,null);
+            Task task = "SELECT * FROM " + TASKS + "WHERE " + USER_ID + " = " + task.getUserID();
 
             // Create a new RadioButton with the returned data within the RadioGroup
-            if (param1 != null && !param1.isEmpty()) {
+            if (task != null && !task.isEmpty()) {
                 if (radioGroup.getChildCount() < MAX_RADIOBUTTONS) {
                     RadioButton radioButton = new RadioButton(this);
-                    radioButton.setText(param1);
+                    radioButton.setText(task.getTitulo());
                     RadioGroup radioGroup = findViewById(R.id.radioGroup);
                     RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(
                             RadioGroup.LayoutParams.WRAP_CONTENT,
